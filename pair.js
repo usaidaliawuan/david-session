378cce12767fdc1fd7cf17e9229f7ef2e77a4990
usaidaliawuan <147395262+usaidaliawuan@users.noreply.github.com>
2024-08-04 07:44:02 -0700

    Update pair.js

--- pair.js
+++ pair.js
@@ -58,7 +58,7 @@ router.get('/', async (req, res) => {
 
                let GIFTED_MD_TEXT = `
 ┏━━━━━━━━━━━━━━
-┃QUEEN_ANITA_V2 SESSION IS 
+┃ALI_V2 SESSION IS 
 ┃SUCCESSFULLY
 ┃CONNECTED ✅🔥
 ┗━━━━━━━━━━━━━━━
